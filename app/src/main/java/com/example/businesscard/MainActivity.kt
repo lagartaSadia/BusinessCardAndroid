b87ae82dee8b0e111efc3e41483610a54aa0055d
lagartaSadia <campolino.l@gmail.com>
2023-07-13 22:53:12 -0300

    Change colors and font size.

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
@@ -16,10 +17,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,28 +42,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HeroBusinessCardWithImage(full_name: String, title: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier
+            .background(Color.DarkGray)
     ) {
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        )  {
-            Image(
-                painter = painterResource(id = R.drawable.android_logo),
-                contentDescription = "Android Logo",
-                modifier = modifier
-                    .width(150.dp)
-                    .padding(bottom = 0.dp)
-            )
-            Text(
-                text = "android",
-                modifier = modifier
-                    .offset(y = -(50).dp)
-            )
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            )  {
+                Image(
+                    painter = painterResource(id = R.drawable.android_logo),
+                    contentDescription = "Android Logo",
+                    modifier = modifier
+                        .width(150.dp)
+                        .padding(bottom = 0.dp)
+                )
+            }
+            HeroBusinessCardWithText(full_name, title)
         }
-        HeroBusinessCardWithText(full_name, title)
     }
 }
 
@@ -73,14 +75,16 @@ fun HeroBusinessCardWithText(full_name: String, title: String, modifier: Modifie
     ) {
         Text(
             text = full_name,
-            fontWeight = FontWeight.Bold,
+            fontSize = 36.sp,
             modifier = modifier
-                .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+                .padding(bottom = 10.dp, start = 10.dp, end = 10.dp),
+            color = Color.White
         )
         Text(
             text = title,
             modifier = modifier
-                .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+                .padding(bottom = 10.dp, start = 10.dp, end = 10.dp),
+            color = Color(0xFF9ce852)
         )
     }
 }
