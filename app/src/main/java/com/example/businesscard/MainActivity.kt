40b86745c70000c262d45ab2c94432d469142800
lagartaSadia <campolino.l@gmail.com>
2023-07-16 02:55:41 -0300

    Contact Information finished

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -8,14 +8,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -49,11 +55,16 @@ fun BusinessCardApp(modifier: Modifier = Modifier) {
             .fillMaxSize(1f)
             .background(Color.DarkGray)
     ) {
-        Box(
-            modifier = modifier
-                .align(Alignment.Center)
-        ) {
-            HeroBusinessCardWithImage(stringResource(R.string.full_name), stringResource(R.string.title))
+        Column {
+            HeroBusinessCardWithImage(
+                stringResource(R.string.full_name),
+                stringResource(R.string.title)
+            )
+            ContactInformationWithText(
+                stringResource(R.string.phone_number),
+                stringResource(R.string.github),
+                stringResource(R.string.email)
+            )
         }
     }
 }
@@ -61,12 +72,15 @@ fun BusinessCardApp(modifier: Modifier = Modifier) {
 @Composable
 fun HeroBusinessCardWithImage(full_name: String, title: String, modifier: Modifier = Modifier) {
     Column(
+        modifier = modifier
+            .fillMaxWidth(1f)
+            .fillMaxHeight(0.75f),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = CenterHorizontally
     ) {
         Column(
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = CenterHorizontally
         )  {
             Image(
                 painter = painterResource(id = R.drawable.android_logo),
@@ -84,7 +98,7 @@ fun HeroBusinessCardWithImage(full_name: String, title: String, modifier: Modifi
 fun HeroBusinessCardWithText(full_name: String, title: String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = CenterHorizontally
     ) {
         Text(
             text = full_name,
@@ -97,8 +111,80 @@ fun HeroBusinessCardWithText(full_name: String, title: String, modifier: Modifie
             text = title,
             modifier = modifier
                 .padding(bottom = 10.dp, start = 10.dp, end = 10.dp),
-            color = Color(0xFF9ce852)
+            color = Color(0xFF3DDC84)
         )
+    }
+}
+
+@Composable
+fun ContactInformationWithText(phone: String, github: String, email: String) {
+    Column {
+        Divider(
+            color = Color.Gray,
+            modifier = Modifier
+                .height(1.dp)
+        )
+        Row(
+            modifier = Modifier
+                .padding(top = 10.dp, bottom = 10.dp)
+                .padding(start = 50.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.phone_call),
+                contentDescription = "Phone icon",
+                tint = Color(0xFF3DDC84)
+            )
+            Text(
+                text = phone,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(start = 10.dp)
+            )
+        }
+        Divider(
+            color = Color.Gray,
+            modifier = Modifier
+                .height(1.dp)
+        )
+        Row(
+            modifier = Modifier
+                .padding(top = 10.dp, bottom = 10.dp)
+                .padding(start = 50.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.github),
+                contentDescription = "Github icon",
+                tint = Color(0xFF3DDC84)
+            )
+            Text(
+                text = github,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(start = 10.dp)
+            )
+        }
+        Divider(
+            color = Color.Gray,
+            modifier = Modifier
+                .height(1.dp)
+        )
+        Row(
+            modifier = Modifier
+                .padding(top = 10.dp, bottom = 10.dp)
+                .padding(start = 50.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.email),
+                contentDescription = "Email icon",
+                tint = Color(0xFF3DDC84)
+            )
+            Text(
+                text = email,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(start = 10.dp)
+            )
+        }
     }
 }
 
